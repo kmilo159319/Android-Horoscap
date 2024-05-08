33075005694653e77c74377848dd3f6c4f03836c
TFHKACOL0\jyepes <jyepes@thefactoryhka.com>
2024-05-08 09:25:53 -0500

    initial commit

---
+++ app/src/main/java/com/kmiloprojects/horoscap/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.kmiloprojects.horoscap.horoscapp.ui.home
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.kmiloprojects.horoscap.R
+import com.kmiloprojects.horoscap.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+
+}
---
+++ app/src/main/java/com/kmiloprojects/horoscap/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,23 @@
+package com.kmiloprojects.horoscap.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.kmiloprojects.horoscap.R
+import com.kmiloprojects.horoscap.databinding.FragmentHoroscopeBinding
+import android.view.View as View1
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState:  Bundle?
+    ): View1 {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
